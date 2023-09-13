9287c56d40d463e103810df6b7a67b324cc9dfc7
Péter Kardos <kardospeter1994@hotmail.com>
2023-09-13 12:48:15 +0200

    xscale yscale

--- utility/line_graph.py
+++ utility/line_graph.py
@@ -3,20 +3,29 @@ from typing import Optional
 import pathlib
 
 
-# You data here
+# Your data here
 #   Never commit these changes!
-xlabel: str = "Block size (*64 bytes)"
-ylabel: str = "Bandwidth (GB/s)"
+xlabel: str = "..."
+ylabel: str = "..."
 title: Optional[str] = None
 xdata = [1, 2]
 ydata = [1, 2]
+xscale: Optional[str] = None # linear, log
+yscale: Optional[str] = None # linear, log
 
 
 # Display graph
 plt.figure()
-xticks = list(range(0, len(ydata)))
-plt.plot(xticks, ydata, "ro-")
-plt.xticks(ticks=xticks, labels=xdata, rotation=35)
+
+if not xscale:
+    xticks = list(range(0, len(ydata)))
+    plt.plot(xticks, ydata, "ro-")
+    plt.xticks(ticks=xticks, labels=xdata, rotation=35)
+else:
+    plt.plot(xdata, ydata, "ro-")
+    plt.xscale(xscale)
+plt.yscale(yscale or "linear")
+
 plt.xlabel(xlabel)
 plt.ylabel(ylabel)
 if title is not None:
