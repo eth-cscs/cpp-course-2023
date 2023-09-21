7932de18d9a324a89ee498ff0035ea88a6f118e4
Mikael Simberg <mikael.simberg@iki.fi>
2023-09-21 17:30:05 +0200

    Also build html slides files through CMake

--- cmake/Marp.cmake
+++ cmake/Marp.cmake
@@ -2,22 +2,36 @@ find_program(MARP_EXECUTABLE "marp" REQUIRED)
 
 function(add_marp_slides name source)
     find_file(source_full ${source} PATHS ${CMAKE_CURRENT_LIST_DIR} NO_CACHE)
-
     set(source ${source_full})
-    set(output "${CMAKE_BINARY_DIR}/${name}.pdf")
+
+    set(output_pdf "${CMAKE_BINARY_DIR}/${name}.pdf")
+    set(output_html "${CMAKE_BINARY_DIR}/${name}.html")
 
     add_custom_command(
-        OUTPUT ${output}
+        OUTPUT ${output_pdf}
+        COMMAND
+            ${MARP_EXECUTABLE}
+            --allow-local-files
+            --html
+            --theme ${CMAKE_SOURCE_DIR}/slides-support/themes/cscs.css
+            --pdf
+            -o ${output_pdf}
+            ${source}
+        MAIN_DEPENDENCY ${source}
+    )
+
+    add_custom_command(
+        OUTPUT ${output_html}
         COMMAND 
             ${MARP_EXECUTABLE} 
             --allow-local-files
             --html
             --theme ${CMAKE_SOURCE_DIR}/slides-support/themes/cscs.css
-            --pdf
-            -o ${output}
+            -o ${output_html}
             ${source}
         MAIN_DEPENDENCY ${source}
     )
-    
-    add_custom_target(${name} ALL DEPENDS ${output})
+
+    add_custom_target(${name}_pdf ALL DEPENDS ${output_pdf})
+    add_custom_target(${name}_html ALL DEPENDS ${output_html})
 endfunction()
