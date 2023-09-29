d3f3d983ccbf3e77dbac7b583035b6ac56ffcde3
Mikael Simberg <mikael.simberg@iki.fi>
2023-09-29 14:42:01 +0200

    Add more examples to lambda and tuple sessions

---
+++ 02_01_lambdas_etc/code/tuple_storage_apply.cpp
@@ -0,0 +1,46 @@
+#include <functional>
+#include <thread>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+
+template <typename F, typename... Ts>
+struct task {
+    std::decay_t<F> f;
+    std::tuple<std::decay_t<Ts>...> t;
+
+    void operator()() {
+        std::apply([&](auto&... ts) {
+            f(ts...);
+        }, t);
+        // or if there are no additional arguments to pass to f simply:
+        // std::apply(f, t);
+    }
+};
+
+template <typename F, typename... Ts>
+auto make_task(F&& f, Ts&&... ts) {
+    return task<F, Ts...>(
+        std::forward<F>(f),
+        std::tuple(std::forward<Ts>(ts)...));
+}
+
+void fill(int* array, int n, int x) {
+    for (int i = 0; i < n; ++i) {
+        array[i] = x;
+    }
+}
+
+int main() {
+    int n = 300;
+    std::vector<int> v(n);
+    int x = 42;
+
+    auto t = make_task(fill, v.data(), v.size(), x);
+
+    {
+        // Initialize thread pool etc. to run task
+        std::thread thr(t);
+        thr.join();
+    }
+}
